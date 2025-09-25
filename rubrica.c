f3395e44581da961cd97272661d1ebaeee26b8d1
christian <kadiuchristian8@gmail.com>
2025-09-25 22:38:49 +0200

    basic reading from file

--- rubrica.c
+++ rubrica.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct contatto{
     char nome[50];
     char cognome[50];
     int cellulare;
+    struct contatto* next;
 } contatto;
 
 
-
 int main(){
-    contatto* rubrica;
+    contatto* rubrica = NULL;
+    char no[50];
+    char cogno[50];
+    int cellula;
+
+    FILE* fp = fopen("file.txt", "r");
+    if (fp == NULL){
+        return 1;
+    }
+    int i = 0;
+    while (fscanf(fp, "%s %s %d", &no[0], &cogno[0], &cellula) == 3){
+        contatto* c = (contatto*)malloc(sizeof(contatto));
+        printf("%s\t%s\t%d\n", no, cogno, cellula);
+        strcpy(c->nome, no);
+        strcpy(c->cognome, cogno);
+        c->cellulare = cellula;
+        c->next = NULL;
+    }
+    return 0;
 }
